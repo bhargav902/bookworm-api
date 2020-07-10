7cba7759a7a299740335d9998f61b70c234b9688
bhargav-busem <bhargav902@gmail.com>
2020-07-10 11:07:39 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import dotenv from 'dotenv';
 import Promise from 'bluebird';
 
 import auth from "./routes/auth";
+import users from "./routes/users";
+import books from "./routes/books";
+
 
 dotenv.config();
 const app = express();
@@ -15,10 +18,12 @@ mongoose.connect(process.env.MONGODB_URL, { useMongoClient: true });
 
 
 app.use("/api/auth", auth);
+app.use("/api/users", users);
+app.use("/api/books", books);
 
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
 
-app.listen(8080, () => console.log("Running on localhost : 8080"));
+app.listen(8082, () => console.log("Running on localhost : 8082"));
